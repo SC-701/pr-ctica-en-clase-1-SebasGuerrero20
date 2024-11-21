c5658bf7df9ef09120613839ba1cfa5a1f228caa
sukencio <sebgueacu@gmail.com>
2024-11-20 20:03:04 -0600

    commit arreglos

--- Práctica con errores/Persona.API/Abstracciones/Modelos/Correo.cs
+++ Práctica con errores/Persona.API/Abstracciones/Modelos/Correo.cs
@@ -8,16 +8,16 @@ namespace Abstracciones.Modelos
 {
     public class Correo
     {
-        public string Asunto { get; set; }
-        public string Cuerpo { get; set; }
-        public string Destinatario { get; set; }
+        public string? Asunto { get; set; }
+        public string? Cuerpo { get; set; }
+        public string? Destinatario { get; set; }
         public IEnumerable<DocumentoContenido>? Adjuntos { get; set; }
     }
     public class CorreoConfiguracion
     {
-        public string Servidor { get; set; }
-        public string Usuario { get; set; }
-        public string Contrasena { get; set; }
+        public string? Servidor { get; set; }
+        public string? Usuario { get; set; }
+        public string? Contrasena { get; set; }
         public int Puerto { get; set; }
     }
 }
